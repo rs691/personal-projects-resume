0456951d751a26b3ed3e72423dc3171af060e445
rs691 <robstewart@my365.bellevue.edu>
2024-10-25 22:50:30 -0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import MainParallax from './components/MainParallax.jsx';
+
+function App() {
+ 
+
+  return (
+    <>
+   <MainParallax />
+    </>
+  )
+}
+
+export default App
