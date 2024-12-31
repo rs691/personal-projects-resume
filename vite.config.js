d859f9aa5bb28bcaa31a7ee9f6ca504383f24ba1
rs691 <robstewart@my365.bellevue.edu>
2024-12-31 00:24:12 -0600

    fixing homepage again

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/personal-projects-resume',
+  base: '/personal-projects-resume/',
   plugins: [react()],
 
 })
